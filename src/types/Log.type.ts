3d31a96a592316b3195cd9a1a59ba934fcb8110d
leylasaeedy <l.saeedy1998@gmail.com>
2025-08-16 13:18:16 +0330

    feat: packages, types files

---
+++ src/types/Log.type.ts
@@ -0,0 +1,5 @@
+export type Log = {
+  action: string;
+  timestamp: string;
+  meta?: object;
+};
---
+++ src/types/user.type.ts
@@ -0,0 +1,35 @@
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export type AuthUser = {
+  email: string;
+  password: string;
+};
+
+export type RegisterFormUser = {
+  email: string;
+  password: string;
+  name: string;
+  username: string;
+};
